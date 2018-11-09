a876b3fb08731b85ff289b4a2ec558409e2fe175
Leonardo Dias <leonarosd.ti@gmail.com>
2018-11-09 13:27:52 -0300

    Criação das entidades relacionadas ao estoque.

---
+++ src/main/java/veterinaria/estoque/model/entidades/Entrada.java
@@ -0,0 +1,99 @@
+package veterinaria.estoque.model.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Entrada implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private Date dataEntrada;
+	
+	private Usuario usuario;
+	
+	private Produto produto;
+	
+	private Lote lote;
+	
+	private int quantidade;
+	
+	public Entrada() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Lote getLote() {
+		return lote;
+	}
+
+	public void setLote(Lote lote) {
+		this.lote = lote;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entrada other = (Entrada) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/veterinaria/estoque/model/entidades/Lote.java
@@ -0,0 +1,69 @@
+package veterinaria.estoque.model.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Lote implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private Long numero;
+	
+	private Date dataValidade;
+	
+	public Lote() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(Date dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lote other = (Lote) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
